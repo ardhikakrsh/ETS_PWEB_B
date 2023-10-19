8232dc7a8024b061e8727b5c0b5b6bcf5efb1a64
ardhikakrsh <ardhikakrishna@gmail.com>
2023-10-20 04:46:04 +0700

    ETS Done

---
+++ script.js
@@ -0,0 +1,5 @@
+var form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+  var name = document.getElementById("name").value;
+  var comment = document.getElementById("comment").value;
+});
